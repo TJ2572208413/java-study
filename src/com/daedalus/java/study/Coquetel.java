a4850ea01d7cd37b3f8aae1f26b0bfc1248b8cbf
daedalus <swbcn2013@gmail.com>
2017-06-06 15:11:56 +0800

    init

--- src/com/daedalus/java/study/Coquetel.java
+++ src/com/daedalus/java/study/Coquetel.java
@@ -3,12 +3,25 @@ package com.daedalus.java.study;
 public class Coquetel {
 
     public static void main(String[] args) {
-        
-        hello();
+
+        String result1 = buy(60);
+        System.out.println(result1);
+
+        String result2 = buy(40);
+        System.out.println(result2);
     }
 
     public static void hello() {
 
         System.out.println("Hello World");
+    }
+
+    public static String buy(int money) {
+
+        if (money > 50) {
+            return "剁手成功";
+        } else {
+            return "钱不够";
+        }
     }
 }
