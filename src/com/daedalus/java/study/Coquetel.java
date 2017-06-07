031fd8ffff755d1174b1e40ad100d41c67e85ed9
daedalus <swbcn2013@gmail.com>
2017-06-07 15:07:45 +0800

    添加Person类

--- src/com/daedalus/java/study/Coquetel.java
+++ src/com/daedalus/java/study/Coquetel.java
@@ -5,6 +5,12 @@ public class Coquetel {
     public static void main(String[] args) {
 
         Person myPerson = new Person();
+        myPerson.hello();
 
+        String result1 = myPerson.buy(60);
+        System.out.println(result1);
+
+        String result2 = myPerson.buy(40);
+        System.out.println(result2);
     }
 }
