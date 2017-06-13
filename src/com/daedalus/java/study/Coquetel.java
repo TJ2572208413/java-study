3f0519e3a9c4431345ac0151be398d23aed38666
daedalus <swbcn2013@gmail.com>
2017-06-13 14:53:02 +0800

    新加 范例

--- src/com/daedalus/java/study/Coquetel.java
+++ src/com/daedalus/java/study/Coquetel.java
@@ -8,29 +8,22 @@ public class Coquetel {
 
     public static void main(String[] args) {
 
-        Animal myAnimal = new Cat("咪咪", "黄色");
-        myAnimal.shout();
+        add();
 
-        myAnimal = new Dog("小白", "白色");
-        myAnimal.shout();
+//        Animal myAnimal = new Cat("咪咪", "黄色");
+//        myAnimal.shout();
+//
+//        myAnimal = new Dog("小白", "白色");
+//        myAnimal.shout();
 
-        //String name1 = "Coquetel";
-
-        //Person myPerson1 = new Person(name1);
-        //myPerson1.hi();
-
-        //String name2 = "Daedalus";
-        //Person myPerson2 = new Person();
-        //myPerson2.setName(name2);
-        //myPerson2.hi();
-
-
-        //Utils.hello();
     }
 
     public static void add() {
+
         Scanner reader = new Scanner(System.in);
+
         System.out.println("输入第一个数字：");
+
         Integer s1 = reader.nextInt();
         System.out.println("你输入的是：" + s1);
 
